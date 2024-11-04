11d43be1c13d2ef70dc96fd1e98694d19752093f
Neeti <neet74215@gmail.com>
2024-11-04 14:10:06 +0530

    Add files via upload

---
+++ QUESTIONS.sql
@@ -0,0 +1,18 @@
+-- MONDAY COFFEE --
+Easy-Medium Questions
+
+1. How many people in each city are estimated to consume coffee, given that 25% of the popukation does?
+2. What is the total revenue generated from coffee sales across all cities in last qtr of 2023?
+3. How many units of each coffee product have been sold?
+4. What is the average sales amount per customer in ach city?	
+5. --city population and coffe consumers(25%)--
+   --Provide a list of cities along with their populaions and estimated coffee consumers.--
+   --return city name, total current customers, estimated coffee consumers(25%)--
+6. What are the top 3 selling products in each city based on sales volume?
+7. How many unique customers are there in each city who have purchased coffee products?
+8. Find each city and their average sale per customer and avg rent per customer
+9. Calculate the percentage growth (or decline) in sales over different time periods (monthly) by city.
+10. Identify top 3 city based on highest sales, return city name, 	total sale, total rent,
+	total customers, estimated coffee consumer	
+
+
